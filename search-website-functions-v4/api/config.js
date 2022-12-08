6fbf894452379f7d2ce96c28ad7fe19555b5d89a
Olga Rudenko <Olga.Rudenko@altusgroup.com>
2022-12-08 13:24:01 -0500

    changed to query key

--- search-website-functions-v4/api/config.js
+++ search-website-functions-v4/api/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     SearchIndexName: process.env["SearchIndexName"] || "good-books",
-    SearchApiKey: process.env["SearchApiKey"] || "F8C70CFA8C7BF87084411E25E4ED29C8",
+    SearchApiKey: process.env["SearchApiKey"] || "29481C4EC76A7059D7F69F89E4BA0143",
     SearchServiceName: process.env["SearchServiceName"] || "cogsrch-aginodev01-cace-001",
     SearchFacets: process.env["SearchFacets"] || "authors*,language_code", 
 }
