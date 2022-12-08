e8466e607b95ca0583dbfe1532a039e6a03a1197
Olga Rudenko <Olga.Rudenko@altusgroup.com>
2022-12-07 20:48:13 -0500

    log request config

--- search-website-functions-v4/client/src/config.js
+++ search-website-functions-v4/client/src/config.js
@@ -1,3 +1,12 @@
 const apiBaseUrl = process.env["REACT_APP_API_BASE_URL"];
 console.log(`apiBaseUrl = ${apiBaseUrl}`);
+
+const CONFIG = {
+    SearchIndexName: process.env["SearchIndexName"] || "good-books",
+    SearchApiKey: process.env["SearchApiKey"] || "F8C70CFA8C7BF87084411E25E4ED29C8",
+    SearchServiceName: process.env["SearchServiceName"] || "cogsrch-aginodev01-cace-001",
+    SearchFacets: process.env["SearchFacets"] || "authors*,language_code", 
+}
+console.log(CONFIG);
+
 export default apiBaseUrl;
