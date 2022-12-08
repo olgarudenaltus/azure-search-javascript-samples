89a20819dc6c69044579bf8a65c1e84945d5a0a0
Olga Rudenko <Olga.Rudenko@altusgroup.com>
2022-12-07 20:27:45 -0500

    updated api config

--- search-website-functions-v4/api/config.js
+++ search-website-functions-v4/api/config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
     SearchIndexName: process.env["SearchIndexName"] || "good-books",
-    SearchApiKey: process.env["SearchApiKey"] || "",
-    SearchServiceName: process.env["SearchServiceName"] || "",
+    SearchApiKey: process.env["SearchApiKey"] || "F8C70CFA8C7BF87084411E25E4ED29C8",
+    SearchServiceName: process.env["SearchServiceName"] || "cogsrch-aginodev01-cace-001",
     SearchFacets: process.env["SearchFacets"] || "authors*,language_code", 
 }
 console.log(CONFIG);
