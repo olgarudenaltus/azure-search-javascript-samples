b0aaa5798c28c8e73e99aee870af944e8d5e3580
Olga Rudenko <Olga.Rudenko@altusgroup.com>
2022-12-15 11:11:34 -0500

    aad login

--- search-website-functions-v4/client/src/App/App.js
+++ search-website-functions-v4/client/src/App/App.js
@@ -26,7 +26,7 @@ export default function App() {
 
   // Fetch authentication API & set user state
   async function fetchAuth() {
-    const response = await fetch("/.auth/me");
+    const response = await fetch("/.auth/login/aad");
     if (response) {
       const contentType = response.headers.get("content-type");
       if (contentType && contentType.indexOf("application/json") !== -1) {
